4ba14429e6518a29524b13ce9387e426e83756e2
Luc Suryo <luc@suryo.com>
2023-07-20 00:42:50 -0600

    adjusted output and what to display

--- mod/calculate/calculate.go
+++ mod/calculate/calculate.go
@@ -9,6 +9,7 @@ package calculate
 
 import (
 	"fmt"
+	// "strconv"
 
 	"configurator"
 	"vars"
@@ -21,32 +22,47 @@ func ShowCalc(c *configurator.Config, p *print.Print) {
 	var takeHone float32
 	var taxTotal float32
 	var insuranceTotal int
-	var taxItem string
+	//var taxItem string
+	var taxValue float32
 
-	fmt.Printf("\t%s\n", p.PrintLine(print.Purple, 60))	
+	monthlyCost := (c.CostHouse + c.CostCar)
+
+	fmt.Printf("\t%s Before taxes: %s\n",
+		p.PrintLine(print.Purple, 23),
+		p.PrintLine(print.Purple, 22),
+	)
+
 	p.PrintGreen(fmt.Sprintf("\tYearly salary\t\t  $%12s\n",
 		format.Format(int64(c.Salary))),
 	)
-	p.PrintGreen(fmt.Sprintf("\tBi-Weekly salary\t  $%12s\n",
+	p.PrintGreen(fmt.Sprintf("\tMonthly salary\t\t  $%12s\n",
+		format.Format(int64(c.Salary/12))),
+	)
+	p.PrintGreen(fmt.Sprintf("\tBi-weekly salary\t  $%12s\n",
 		format.Format(int64(c.Salary/24))),
 	)
+	p.PrintGreen(fmt.Sprintf("\tMonthly Cost\t\t  $%12s\n",
+		format.Format(int64(monthlyCost))),
+	)
 
-	fmt.Printf("\n\t%s Federal, State and Medicare: %s\n",
+	fmt.Printf("\t%s Federal, State and Medicare: %s\n",
 		p.PrintLine(print.Purple, 15),
 		p.PrintLine(print.Purple, 15),
 	)
 	// Taxes
 	for _, item := range vars.CalcTax {
-		taxItem = fmt.Sprintf("%.2f", (c.Tax[item] / 100) * (float32(c.Salary/24)))
-		p.PrintYellow(fmt.Sprintf("\t%14s (%v%%)\t  $ %12s\n",
-			item, c.Tax[item], taxItem),
+		// taxItem = fmt.Sprintf("%.2f", (c.Tax[item] / 100) * (float32(c.Salary/24)))
+		taxValue = (c.Tax[item] / 100) * (float32(c.Salary/24))
+		p.PrintYellow(fmt.Sprintf("\t%14s (%v%%)\t  $ %11s\n",
+			item, c.Tax[item],	// taxItem),
+			format.Format(int64(taxValue))),
 		)
 		taxTotal += (c.Tax[item] / 100) * (float32(c.Salary/24))
 	}
 	p.PrintRed(fmt.Sprintf("\tTotal $%s\n", format.Format(int64(taxTotal))))
 	
 	// CalcInsurance
-	fmt.Printf("\n\t%s Insurance and 401K: %s\n",
+	fmt.Printf("\t%s Insurance and 401K: %s\n",
 		p.PrintLine(print.Purple, 20),
 		p.PrintLine(print.Purple, 20),
 	)
@@ -59,11 +75,16 @@ func ShowCalc(c *configurator.Config, p *print.Print) {
 	p.PrintRed(fmt.Sprintf("\tTotal $%s\n", format.Format(int64(insuranceTotal))))
 	fmt.Printf("\t%s\n", p.PrintLine(print.Purple, 60))	
 
-	monthlyCost := (c.CostHouse + c.CostCar)/ 2 
 	takeHone =	float32(c.Salary/24) - taxTotal - float32(insuranceTotal)
-	afterCost := (takeHone *2) - float32(monthlyCost)
+	afterCost := takeHone - float32(monthlyCost/ 2)
 
-	p.PrintGreen(fmt.Sprintf("\tBring home salary $%s bi-weekly\n", format.Format(int64(takeHone))))
-	p.PrintGreen(fmt.Sprintf("\tAfter monthly costs $%s ($%s/monthly before cost)\n",
-			format.Format(int64(afterCost)), format.Format(int64(takeHone * 2))))
+	p.PrintGreen(fmt.Sprintf("\tBring home salary ~$%s bi-weekly, ~$%s monthly\n",
+		format.Format(int64(takeHone)),
+		format.Format(int64(takeHone * 2))),
+	)
+
+	p.PrintGreen(fmt.Sprintf("\tAfter costs ~$%s bi-weekly, ~$%s monthly\n",
+			format.Format(int64(afterCost)),
+			format.Format(int64(afterCost * 2))),
+	)
 }
